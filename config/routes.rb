4d6de4360799a690e1c74265a9c86c30c8654c3a
Ryan Melton <ryanmelt@gmail.com>
2013-06-01 10:34:24 -0600

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Blogit::Engine.routes.draw do
   # Keep these above the posts resources block
   get "posts/page/:page" => "posts#index"
   get "posts/tagged/:tag" => 'posts#tagged', as: :tagged_blog_posts
-  get "posts/:id/edit" => 'posts#edit'
-  get "posts/:id/*other" => 'posts#show'
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
+
+  get "posts/:id/*other" => 'posts#show'
 
   match "/" => "posts#index", as: :blog_root
 end
